614d6c05829a8d3db1fa94dd5d2b7508442f77b8
Tijesunimi Peters <tijesunimi.peters@andela.com>
2017-02-22 07:32:17 +0100

    working on the pug inclusion

--- app/controllers/pages_controller.php
+++ app/controllers/pages_controller.php
@@ -19,14 +19,13 @@ class PagesController extends BaseController
 	}
 
 	public function postMail($params) {
-		dd(unserialize($params['checkout']));
 		$value = unserialize($params['checkout']);
 		$headers = [];
 		$headers[] = 'MIME-Version: 1.0';
 		$headers[] = 'Content-type: text/html; charset=UTF-8';
 		$headers[] = 'From: New Shipping <noreply@gcl.com>';
 		$to = $value['mail_from'].", ". $value['mail_to'];
-		if(mail($to,'New Shipping', $this->render('layouts/mail', $params), implode("\r\n",$headers))) {
+		if(mail($to,'New Shipping', $this->render('layouts/mail', $value), implode("\r\n",$headers))) {
 			return $this->render('404', ['message'=>"Thank you for using our service;\nAn email has been sent to you"]);
 		} else {
 			return $this->render('404', ['message'=>"Something went wrong sending your mail;\nPlease try again"]);
